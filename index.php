34b79a01404ef7fdf0b8b9df87fa14526f4bb624
Karol Preiskorn <karol.preiskorn@gmail.com>
2017-12-09 11:26:39 +0100

    poprawki

--- index.php
+++ index.php
@@ -18,53 +18,62 @@ use ZabbixApi\ZabbixApi;
  * @var string $uri
  *
  */
-$uri = "http://wrex.oss.t-mobile.pl/zabbix/api_jsonrpc.php";
-$username = "inact_ro";
-$password = "Karol!123";
+
+$uri = "http://zabixx";
+$username = "user";
+$password = "pass";
+
+// set passwords and uri
+include "passwords.php";
+
 /**
  * poor print_r function
  *
  * @param unknown $val
  *
  */
-function print_r2($val) {
-	echo '<pre>';
-	print_r ( $val );
-	echo '</pre>';
+function print_r2($val)
+{
+    echo '<pre>';
+    print_r($val);
+    echo '</pre>';
 }
+
 /**
  * Better print_r function
  */
-function obsafe_print_r($var, $return = false, $html = true, $level = 0) {
-	$spaces = "";
-	$space = $html ? "&nbsp;" : " ";
-	$newline = $html ? "<br />" : "\n";
-	for($i = 1; $i <= 6; $i ++) {
-		$spaces .= $space;
-	}
-	$tabs = $spaces;
-	for($i = 1; $i <= $level; $i ++) {
-		$tabs .= $spaces;
-	}
-	if (is_array ( $var )) {
-		$title = "Array";
-	} elseif (is_object ( $var )) {
-		$title = get_class ( $var ) . " Object";
-	}
-	$output = "<b>" . $title . "</b>" . $newline . $newline;
-	foreach ( $var as $key => $value ) {
-		if (is_array ( $value ) || is_object ( $value )) {
-			$level ++;
-			$value = obsafe_print_r ( $value, true, $html, $level );
-			$level --;
-		}
-		$output .= $tabs . "[" . $key . "] => [" . $value . "]" . $newline;
-	}
-	if ($return)
-		return $output;
-	else
-		echo "<pre>" . $output . "</pre>";
+function obsafe_print_r($var, $return = false, $html = true, $level = 0)
+{
+    $spaces = "";
+    $space = $html ? "&nbsp;" : " ";
+    $newline = $html ? "<br />" : "\n";
+    for ($i = 1; $i <= 6; $i ++) {
+        $spaces .= $space;
+    }
+    $tabs = $spaces;
+    for ($i = 1; $i <= $level; $i ++) {
+        $tabs .= $spaces;
+    }
+    if (is_array($var)) {
+        $title = "Array";
+    } elseif (is_object($var)) {
+        $title = get_class($var) . " Object";
+    }
+    $output = "<b>" . $title . "</b>" . $newline . $newline;
+    foreach ($var as $key => $value) {
+        if (is_array($value) || is_object($value)) {
+            $level ++;
+            $value = obsafe_print_r($value, true, $html, $level);
+            $level --;
+        }
+        $output .= $tabs . "[" . $key . "] => [" . $value . "]" . $newline;
+    }
+    if ($return)
+        return $output;
+    else
+        echo "<pre>" . $output . "</pre>";
 }
+
 /**
  *
  * Generate bootstrap table with headers and coun of elements
@@ -73,82 +82,83 @@ function obsafe_print_r($var, $return = false, $html = true, $level = 0) {
  * @param unknown $count
  * @param unknown $id
  */
-function print_table_header($title, $count, $headers, $id) {
-	print ('<div class="panel panel-default">' . "\n") ;
-	print ('<div class="panel-heading">' . $title . ' <a href="#"> <span class="badge">' . $count . '</span></a></div>' . "\n") ;
-	print ("<table id='" . $id . "' class='table table-striped table-bordered'>" . "\n") ;
+function print_table_header($title, $count, $headers, $id)
+{
+    print('<div class="panel panel-default">' . "\n");
+    print('<div class="panel-heading">' . $title . ' <a href="#"> <span class="badge">' . $count . '</span></a></div>' . "\n");
+    print("<table id='" . $id . "' class='table table-striped table-bordered'>" . "\n");
 
-	print ("<thead><tr>" . "\n") ;
-	foreach ( $headers as $header ) {
-		print ("<th>" . $header . "</th>" . "\n") ;
-	}
-	print ("</tr>\n</thead>\n<tbody>" . "\n") ;
+    print("<thead><tr>" . "\n");
+    foreach ($headers as $header) {
+        print("<th>" . $header . "</th>" . "\n");
+    }
+    print("</tr>\n</thead>\n<tbody>" . "\n");
 }
 
 try {
-	// connect to Zabbix API
-	$api = new ZabbixApi ( $uri, $username, $password );
+    // connect to Zabbix API
+    $api = new ZabbixApi($uri, $username, $password);
 
-	// get all graphs
-	$alerts = $api->alertGet ();
-	// print_r2 ( $graphs [0] );
+    // get all graphs
+    $alerts = $api->alertGet();
+    // print_r2 ( $graphs [0] );
 
-	arsort ( $alerts );
+    arsort($alerts);
 
-	$headers = array (
-			"Alert Id",
-			"Event Id",
-			"Date time",
-			"Sent to",
-			"Subject",
-			"Message"
-	);
+    $headers = array(
+        "Alert Id",
+        "Event Id",
+        "Date time",
+        "Sent to",
+        "Subject",
+        "Message"
+    );
 
-	print_table_header ( 'Alerts', count ( $alerts ), $headers, 'alarms' );
+    print_table_header('Alerts', count($alerts), $headers, 'alarms');
 
-	// print all alerts
-	foreach ( $alerts as $alert ) {
-		if ($alert->sendto = "karol.preiskorn@t-mobile.pl") {
-			if (strpos ( $alert->subject, "PROBLEM:" ) !== false) {
-				$alerttype = "class='warning'";
-			} else {
-				$alerttype = "class='normal'";
-			}
-			if (preg_match ( "/Original event ID: ([0-9]{4,12})/", $alert->message, $regs )) {
-				$eventid_org = "[link: <a href='#" . $regs [1] . "'>" . $regs [1] . "</a>]";
-			} else {
-				$eventid_org = "Not found Original event ID";
-			}
-			printf ( "<tr id='%s' %s><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" . "\n", $alert->eventid, $alerttype, $alert->alertid, $alert->eventid, date ( "Y-m-d h:i", $alert->clock ), $alert->sendto, $alert->subject, $alert->message . " " . $eventid_org );
-		}
-	}
-	print ("</tbody>\n</table>\n</div>\n") ;
+    // print all alerts
+    foreach ($alerts as $alert) {
+        if ($alert->sendto != "karol.preiskorn@t-mobile.pl") {
+            if (strpos($alert->subject, "PROBLEM:") !== false) {
+                $alerttype = "class='warning'";
+            } else {
+                $alerttype = "class='normal'";
+            }
+            if (preg_match("/Original event ID: ([0-9]{4,12})/", $alert->message, $regs)) {
+                $eventid_org = "[link: <a href='#" . $regs[1] . "'>" . $regs[1] . "</a>]";
+            } else {
+                $eventid_org = "Not found Original event ID";
+            }
+            printf("<tr id='%s' %s><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>" . "\n", $alert->eventid, $alerttype, $alert->alertid, $alert->eventid, date("Y-m-d h:i", $alert->clock), $alert->sendto, $alert->subject, $alert->message . " " . $eventid_org);
+        }
+    }
+    print("</tbody>\n</table>\n</div>\n");
 
-	// get all graphs
-	$graphs = $api->graphGet ();
-	$header_graph = array (
-			"Graph Id",
-			"Description"
-	);
-	print_table_header ( 'Graph', count ( $graphs ), $header_graph, 'graph' );
+    // get all graphs
+    $graphs = $api->graphGet();
+    $header_graph = array(
+        "Graph Id",
+        "Description"
+    );
+    print_table_header('Graph', count($graphs), $header_graph, 'graph');
 
-	// print all graph IDs
-	foreach ( $graphs as $graph ) {
-		printf ( "<tr><td>%8d</td><td>%s</td></tr>\n", $graph->graphid, $graph->name );
-	}
-	print ("</tbody>\n</table>\n</div>\n") ;
+    // print all graph IDs
+    foreach ($graphs as $graph) {
+        printf("<tr><td>%8d</td><td>%s</td></tr>\n", $graph->graphid, $graph->name);
+    }
+    print("</tbody>\n</table>\n</div>\n");
 
-	print ('<div class="panel-footer">
+    print('<div class="panel-footer">
 
         <p>&copy; T-Mobile Polska Company | OSS development powered by Karol Preiskorn | internal use</p>
 
-      </div>') ;
-	print ("</div></body></html>") ; // end of container
+      </div>');
+    print("</div></body></html>"); // end of container
 
-	// print "<h1>Alerts... agian in raw</h2>";
-		                                 // obsafe_print_r ( $alerts, FALSE, TRUE );
-} catch ( Exception $e ) {
-	// Exception in ZabbixApi catched
-	echo $e->getMessage ();
+    // print "<h1>Alerts... agian in raw</h2>";
+                                       // obsafe_print_r ( $alerts, FALSE, TRUE );
+} catch (Exception $e) {
+    // Exception in ZabbixApi catched
+    echo $e->getMessage();
 }
 ?>
